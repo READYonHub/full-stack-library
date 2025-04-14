09f3f08899a8ecec7c5778759d87c1b65b586a7a
READYonHub <readyemail3@gmail.com>
2025-04-14 15:23:23 +0200

    1. home layout

---
+++ components/BookOverview.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BookOverview = () => {
+    return (
+        <section className='book-overview'>
+            <div className='flex flex-1 flex-col gap-5'>
+                <h1 className='text-3xl font-bold'>Book Overview</h1>
+            </div>
+        </section>
+    )
+}
+
+export default BookOverview
